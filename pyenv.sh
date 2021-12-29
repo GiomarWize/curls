ce34a7c50a86de4d81dd210909d03624914033d8
Giomar Osorio <95301399+GiomarWize@users.noreply.github.com>
2021-12-29 18:46:40 -0500

    Update pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -1,14 +1,31 @@
-#!/bin/bash
-set -e
-# Updated Script
-# Vars
+#!/usr/bin/env bash
+#
+#    Licensed to the Apache Software Foundation (ASF) under one or more
+#    contributor license agreements.  See the NOTICE file distributed with
+#    this work for additional information regarding copyright ownership.
+#    The ASF licenses this file to You under the Apache License, Version 2.0
+#    (the "License"); you may not use this file except in compliance with
+#    the License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+#    Python version 3.6.13-3.7.10-3.8.9-3.9.4 installer via pyenv.
+#
+set -euo pipefail
+
 pyenv_dependencies="make build-essential libssl-dev zlib1g-dev bzip2 libreadline-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
 python_versions_arr=("3.6.13" "3.7.10" "3.8.9" "3.9.4")
 
-# Install pyenv dependencies
 pyenv_dep(){
  sudo apt-get install -y $1
 }
+
 pyenv_install(){
   if [[ -d $HOME/.pyenv/ ]]; then
     sudo rm -r $HOME/.pyenv
@@ -20,21 +37,22 @@ pyenv_install(){
   chmod +x $HOME/pyenv_installer.sh
   $HOME/pyenv_installer.sh
 }
-pyenv_post(){
+
+pyenv_post_install(){
  echo '' >> $HOME/.bashrc
  echo '# pyenv Config' >> $HOME/.bashrc
  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.bashrc
  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.bashrc
  echo 'if which pyenv > /dev/null;then eval "$(pyenv init -)"; eval "$(pyenv init --path)"; eval "$(pyenv virtualenv-init -)"; fi' >> $HOME/.bashrc
 }
-# Install python versions
+
 pyenv_versions_install(){
  arr=("$@")
 for version in "${arr[@]}"; do
   $HOME/.pyenv/bin/pyenv install $version
 done
 }
-# Setting globally
+
 python_versions_setglobally(){
  arr=("$@")
  versions=""
@@ -46,11 +64,13 @@ done
  echo "${versions}"
  $HOME/.pyenv/bin/pyenv global "${versions[@]}"
 }
+
 pyenv(){
  pyenv_dep "${pyenv_dependencies}"
  pyenv_install
+ pyenv_post_install
  pyenv_versions_install "${python_versions_arr[@]}"
  python_versions_setglobally "${python_versions_arr[@]}"
- pyenv_post
 }
+
 pyenv
