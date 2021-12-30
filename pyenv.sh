14f80e3b266d71b6a5906a2c73d7f01c1d4aa6a7
Giomar Osorio <95301399+GiomarWize@users.noreply.github.com>
2021-12-29 20:52:46 -0500

    Update pyenv.sh, shellcheck

--- pyenv.sh
+++ pyenv.sh
@@ -19,50 +19,58 @@
 #
 set -euo pipefail
 
-pyenv_dependencies="make build-essential libssl-dev zlib1g-dev bzip2 libreadline-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev"
+pyenv_dependencies="make build-essential libssl-dev zlib1g-dev bzip2 \
+libreadline-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev \
+liblzma-dev"
 python_versions_arr=("3.6.13" "3.7.10" "3.8.9" "3.9.4")
 
 pyenv_dep(){
- sudo apt-get install -y $1
+ sudo apt-get install -y "$1"
 }
 
 pyenv_install(){
-  if [[ -d $HOME/.pyenv/ ]]; then
-    sudo rm -r $HOME/.pyenv
+  if [[ -d "$HOME"/.pyenv/ ]]; then
+    sudo rm -r "$HOME"/.pyenv
   fi
-  if [[ -e $HOME/pyenv_installer.sh ]]; then
-    sudo rm $HOME/pyenv_installer.sh
+  if [[ -e "$HOME"/pyenv_installer.sh ]]; then
+    sudo rm "$HOME"/pyenv_installer.sh
   fi
-  curl https://pyenv.run > $HOME/pyenv_installer.sh
-  chmod +x $HOME/pyenv_installer.sh
-  $HOME/pyenv_installer.sh
+  curl https://pyenv.run > "$HOME"/pyenv_installer.sh
+  chmod +x "$HOME"/pyenv_installer.sh
+  "$HOME"/pyenv_installer.sh
 }
 
 pyenv_post_install(){
- echo '' >> $HOME/.bashrc
- echo '# pyenv Config' >> $HOME/.bashrc
- echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.bashrc
- echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.bashrc
- echo 'if which pyenv > /dev/null;then eval "$(pyenv init -)"; eval "$(pyenv init --path)"; eval "$(pyenv virtualenv-init -)"; fi' >> $HOME/.bashrc
+cat <<EOT >> greetings.txt
+
+# pyenv Config
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if which pyenv > /dev/null; then
+  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
+  eval "$(pyenv virtualenv-init -)"
+fi
+EOT
 }
 
 pyenv_versions_install(){
- arr=("$@")
-for version in "${arr[@]}"; do
-  $HOME/.pyenv/bin/pyenv install $version
-done
+  arr=("$@")
+  for version in "${arr[@]}"; do
+    "$HOME"/.pyenv/bin/pyenv install "$version"
+  done
 }
 
 python_versions_setglobally(){
- arr=("$@")
- versions=""
-for ver in "${arr[@]}"; do
-  if [[ -n $($HOME/.pyenv/bin/pyenv versions | grep "${ver}") ]]; then
-    versions+="${ver} "
-  fi
-done
- echo "${versions}"
- $HOME/.pyenv/bin/pyenv global "${versions[@]}"
+  arr=("$@")
+  versions=""
+  for ver in "${arr[@]}"; do
+    if "$HOME"/.pyenv/bin/pyenv versions | grep -q"${ver}"; then
+      versions+="${ver} "
+    fi
+  done
+  "$HOME"/.pyenv/bin/pyenv global "${versions[@]}"
 }
 
 pyenv(){
